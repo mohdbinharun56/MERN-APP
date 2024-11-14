29fb37dd53a1f2fe22e3c82718f15432eb4c3844
mohdbinharun56 <arahaman461@gmail.com>
2024-11-14 10:56:41 +0600

    implementing the route parameter for dynamic routing

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 3000;
 // initialize middleware
 app.use(express.json({extended:false}));
 
+
+// Dynamic Routing Parameter
+app.get('/user/:id',(req,res)=>{
+    res.send(`This is User: ${req.params.id}`) // params parameter will contain the url variable value and the property name also be exact variable name like here used 'id' as dynamic route.
+}) 
+
+// Post Method
 app.post('/add',(req,res)=>{
     res.send(`This is ${req.body.name}`);
 })
