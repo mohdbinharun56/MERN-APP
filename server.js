c96753d7a66cf1bcfb2e40ac8321267c2a446b63
mohdbinharun56 <arahaman461@gmail.com>
2024-11-14 10:44:19 +0600

    implementing how a post request send with body parameter using postman

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+// initialize middleware
+app.use(express.json({extended:false}));
+
+app.post('/add',(req,res)=>{
+    res.send(`This is ${req.body.name}`);
+})
+
+// Get Method 
 app.get('/',(req,res)=>{
     res.send("Hello, Express");
 });
